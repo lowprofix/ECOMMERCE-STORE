ee84c5f8af45b10a1c5fc927a41afb84023b671e
lowprofix <117908259+lowprofix@users.noreply.github.com>
2023-08-01 17:37:01 +0200

    final ecommerce-store

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,44 +1,48 @@
 "use client";
 
-import Link from "next/link"
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { Category } from "@/types";
 
 interface MainNavProps {
   data: Category[];
 }
 
-const MainNav: React.FC<MainNavProps> = ({
-  data
-}) => {
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
 
-  const routes = data.map((route) => ({
-    href: `/category/${route.id}`,
-    label: route.name,
-    active: pathname === `/category/${route.id}`,
-  }));
+  const categoryOrder = [
+    "004707c8-2d8d-44da-a50b-b272dbbc2b0d",
+    "2381e18e-18aa-4bd6-b99b-572795c44625",
+    "22f1e680-97c0-4442-9922-5746f489d18d",
+  ];
+
+  const routes = data
+    .sort((a, b) => categoryOrder.indexOf(a.id) - categoryOrder.indexOf(b.id))
+    .map((route) => ({
+      href: `/category/${route.id}`,
+      label: route.name,
+      active: pathname === `/category/${route.id}`,
+    }));
 
   return (
-    <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
-    >
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
           className={cn(
-            'text-sm font-medium transition-colors hover:text-black',
-            route.active ? 'text-black' : 'text-neutral-500'
+            "text-sm font-medium transition-colors hover:text-black",
+            route.active ? "text-black" : "text-neutral-500"
           )}
         >
           {route.label}
-      </Link>
+        </Link>
       ))}
     </nav>
-  )
+  );
 };
 
 export default MainNav;
